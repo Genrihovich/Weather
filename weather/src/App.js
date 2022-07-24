d2868e2669dd043b417601e0d871720a7a19e9d5
Slaventy1969 <slaventy.69@gmail.com>
2022-07-24 21:22:46 +0300

    Implemented Header

--- weather/src/App.js
+++ weather/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home/components/Home';
 import { MonthStatistics } from './pages/MonthStatistics/components/MonthStatistics';
-import Head from './shared/Header/Header';
+import Header from './shared/Header/Header';
+
 
 function App() {
   return (
-    <>
-      <Head>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/month-statistics" element={<MonthStatistics />} />
-        </Routes>
-      </Head>
-    </>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/month-statistics" element={<MonthStatistics />} />
+      </Routes>
+
+    </div>
   );
 }
 
--- weather/src/shared/Header/Header.js
+++ weather/src/shared/Header/Header.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import s from './Header.module.scss';
+import { GlobalSvgSelector } from '../../assets/icons/global/GlobalSvgSelector';
+import Select from 'react-select';
 
 const Header = () => {
+
+    const options = [
+        { value: 'city-1', label: 'Київ' },
+        { value: 'city-2', label: 'Хмельницький' },
+        { value: 'city-3', label: 'Меджибож' }
+    ];
+    const colourStyles = {
+        control: (styles) => ({
+            ...styles,
+            backgroundColor: 'rgba(71,147,255,0.2)',
+            width: '194px',
+            height: '37px',
+            border: 'none',
+            borderRadius: '10px',
+            zIndex: 100,
+        })
+    }
+
     return (
         <div className={s.header}>
-            Header
+            <div className={s.wrapper}>
+                <div className={s.logo}><GlobalSvgSelector id="header-logo" /></div>
+                <div className={s.title}>React Weather</div>
+            </div>
+            <div className={s.wrapper}>
+                <div className={s.change_theme}><GlobalSvgSelector id="change_theme" /></div>
+                <Select
+                    defaultValue={options[0]}
+                    styles={colourStyles}
+                    options={options}
+                />
+            </div>
         </div>
     )
 }
-
 
 export default Header;
